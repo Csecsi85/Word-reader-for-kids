f24dfab0d22f3f3dfd5f3ea6195e2caa589ac480
Csecsi <csecsi85@gmail.com>
2020-04-16 23:17:46 +0100

    Separated JS files for better readability.

---
+++ js/cardPicker.js
@@ -0,0 +1,13 @@
+document.querySelector('#audio').addEventListener('click', cardPicker);
+
+function cardPicker() {
+	let randomNumber = Math.floor(Math.random() * cardData.length);
+	let randomCard = [cardData[randomNumber].word, cardData[randomNumber].sound];
+	document.querySelector('.card-container-text').innerHTML = randomCard[0];
+	let audioElement = new Audio(randomCard[1]);
+	audioElement.load();
+	audioElement.play();
+	document.getElementById('audio').audio.src = randomCard[1];
+}
+
+cardPicker();
