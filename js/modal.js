ed2655f82f41c09c0caed768d4394499ce247262
Gabor Csecsetka <csecsi85@gmail.com>
2020-04-18 17:52:19 +0100

    Added modal container show up when user click on the settings button

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+let modalButton = document.querySelector('.modal-button');
+
+modalButton.addEventListener('click', () => {
+  modalToggle()
+});
+
+function modalToggle() {
+  document.querySelector('.modal-container').classList.toggle('modal');
+
+};
