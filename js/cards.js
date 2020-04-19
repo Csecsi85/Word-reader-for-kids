4418b435114e464e99a9aaa5746faa3c0646bd61
Gabor Csecsetka <csecsi85@gmail.com>
2020-04-19 03:22:35 +0100

    Connected settings input fields and saved it in a function
    Connected everything in the settings page and added them to a function called: userInput. 
    Commented majority of the code and added to fix comments where work needs to be done.

--- js/cards.js
+++ js/cards.js
@@ -1,386 +1,387 @@
+/* Array of objects with a word and the correspondig file location */
 const cardData = [
 	{
-		word: 'ala',
-		sound: 'assets/audio/ala.m4a',
+		word  : 'ala',
+		sound : 'assets/audio/ala.m4a'
 	},
 	{
-		word: 'angel',
-		sound: 'assets/audio/angel.m4a',
+		word  : 'angel',
+		sound : 'assets/audio/angel.m4a'
 	},
 	{
-		word: 'araña',
-		sound: 'assets/audio/arana.m4a',
+		word  : 'araña',
+		sound : 'assets/audio/arana.m4a'
 	},
 	{
-		word: 'árbol de Navidad',
-		sound: 'assets/audio/arbol-de-navidad.m4a',
+		word  : 'árbol de Navidad',
+		sound : 'assets/audio/arbol-de-navidad.m4a'
 	},
 	{
-		word: 'arena',
-		sound: 'assets/audio/arena.m4a',
+		word  : 'arena',
+		sound : 'assets/audio/arena.m4a'
 	},
 	{
-		word: 'arroz',
-		sound: 'assets/audio/arroz.m4a',
+		word  : 'arroz',
+		sound : 'assets/audio/arroz.m4a'
 	},
 	{
-		word: 'avión',
-		sound: 'assets/audio/avion.m4a',
+		word  : 'avión',
+		sound : 'assets/audio/avion.m4a'
 	},
 	{
-		word: 'banco',
-		sound: 'assets/audio/banco.m4a',
+		word  : 'banco',
+		sound : 'assets/audio/banco.m4a'
 	},
 	{
-		word: 'basura',
-		sound: 'assets/audio/basura.m4a',
+		word  : 'basura',
+		sound : 'assets/audio/basura.m4a'
 	},
 	{
-		word: 'bebida',
-		sound: 'assets/audio/bebida.m4a',
+		word  : 'bebida',
+		sound : 'assets/audio/bebida.m4a'
 	},
 	{
-		word: 'bombero',
-		sound: 'assets/audio/bombero.m4a',
+		word  : 'bombero',
+		sound : 'assets/audio/bombero.m4a'
 	},
 	{
-		word: 'bucear',
-		sound: 'assets/audio/bucear.m4a',
+		word  : 'bucear',
+		sound : 'assets/audio/bucear.m4a'
 	},
 	{
-		word: 'burbuja',
-		sound: 'assets/audio/burbuja.m4a',
+		word  : 'burbuja',
+		sound : 'assets/audio/burbuja.m4a'
 	},
 	{
-		word: 'buzon',
-		sound: 'assets/audio/buzon.m4a',
+		word  : 'buzon',
+		sound : 'assets/audio/buzon.m4a'
 	},
 	{
-		word: 'caballo',
-		sound: 'assets/audio/caballo.m4a',
+		word  : 'caballo',
+		sound : 'assets/audio/caballo.m4a'
 	},
 	{
-		word: 'calcetines',
-		sound: 'assets/audio/calcetines.m4a',
+		word  : 'calcetines',
+		sound : 'assets/audio/calcetines.m4a'
 	},
 	{
-		word: 'cama',
-		sound: 'assets/audio/cama.m4a',
+		word  : 'cama',
+		sound : 'assets/audio/cama.m4a'
 	},
 	{
-		word: 'camello',
-		sound: 'assets/audio/camello.m4a',
+		word  : 'camello',
+		sound : 'assets/audio/camello.m4a'
 	},
 	{
-		word: 'camisa',
-		sound: 'assets/audio/camisa.m4a',
+		word  : 'camisa',
+		sound : 'assets/audio/camisa.m4a'
 	},
 	{
-		word: 'caramelo',
-		sound: 'assets/audio/caramelo.m4a',
+		word  : 'caramelo',
+		sound : 'assets/audio/caramelo.m4a'
 	},
 	{
-		word: 'casa',
-		sound: 'assets/audio/casa.m4a',
+		word  : 'casa',
+		sound : 'assets/audio/casa.m4a'
 	},
 	{
-		word: 'cerdo',
-		sound: 'assets/audio/cerdo.m4a',
+		word  : 'cerdo',
+		sound : 'assets/audio/cerdo.m4a'
 	},
 	{
-		word: 'cereza',
-		sound: 'assets/audio/cereza.m4a',
+		word  : 'cereza',
+		sound : 'assets/audio/cereza.m4a'
 	},
 	{
-		word: 'circo',
-		sound: 'assets/audio/circo.m4a',
+		word  : 'circo',
+		sound : 'assets/audio/circo.m4a'
 	},
 	{
-		word: 'ciudad',
-		sound: 'assets/audio/ciudad.m4a',
+		word  : 'ciudad',
+		sound : 'assets/audio/ciudad.m4a'
 	},
 	{
-		word: 'cocinera',
-		sound: 'assets/audio/cocinera.m4a',
+		word  : 'cocinera',
+		sound : 'assets/audio/cocinera.m4a'
 	},
 	{
-		word: 'colina',
-		sound: 'assets/audio/colina.m4a',
+		word  : 'colina',
+		sound : 'assets/audio/colina.m4a'
 	},
 	{
-		word: 'corona',
-		sound: 'assets/audio/corona.m4a',
+		word  : 'corona',
+		sound : 'assets/audio/corona.m4a'
 	},
 	{
-		word: 'coscillas',
-		sound: 'assets/audio/coscillas.m4a',
+		word  : 'coscillas',
+		sound : 'assets/audio/coscillas.m4a'
 	},
 	{
-		word: 'cuchillo',
-		sound: 'assets/audio/cuchillo.m4a',
+		word  : 'cuchillo',
+		sound : 'assets/audio/cuchillo.m4a'
 	},
 	{
-		word: 'cuna',
-		sound: 'assets/audio/cuna.m4a',
+		word  : 'cuna',
+		sound : 'assets/audio/cuna.m4a'
 	},
 	{
-		word: 'dinero',
-		sound: 'assets/audio/dinero.m4a',
+		word  : 'dinero',
+		sound : 'assets/audio/dinero.m4a'
 	},
 	{
-		word: 'dormir',
-		sound: 'assets/audio/dormir.m4a',
+		word  : 'dormir',
+		sound : 'assets/audio/dormir.m4a'
 	},
 	{
-		word: 'dulce',
-		sound: 'assets/audio/dulce.m4a',
+		word  : 'dulce',
+		sound : 'assets/audio/dulce.m4a'
 	},
 	{
-		word: 'elefante',
-		sound: 'assets/audio/elefante.m4a',
+		word  : 'elefante',
+		sound : 'assets/audio/elefante.m4a'
 	},
 	{
-		word: 'escritorio',
-		sound: 'assets/audio/escritorio.m4a',
+		word  : 'escritorio',
+		sound : 'assets/audio/escritorio.m4a'
 	},
 	{
-		word: 'estomago',
-		sound: 'assets/audio/estomago.m4a',
+		word  : 'estomago',
+		sound : 'assets/audio/estomago.m4a'
 	},
 	{
-		word: 'estrella',
-		sound: 'assets/audio/estrella.m4a',
+		word  : 'estrella',
+		sound : 'assets/audio/estrella.m4a'
 	},
 	{
-		word: 'fantasma',
-		sound: 'assets/audio/fantasma.m4a',
+		word  : 'fantasma',
+		sound : 'assets/audio/fantasma.m4a'
 	},
 	{
-		word: 'farmacia',
-		sound: 'assets/audio/farmacia.m4a',
+		word  : 'farmacia',
+		sound : 'assets/audio/farmacia.m4a'
 	},
 	{
-		word: 'fiesta',
-		sound: 'assets/audio/fiesta.m4a',
+		word  : 'fiesta',
+		sound : 'assets/audio/fiesta.m4a'
 	},
 	{
-		word: 'futbol',
-		sound: 'assets/audio/futbol.m4a',
+		word  : 'futbol',
+		sound : 'assets/audio/futbol.m4a'
 	},
 	{
-		word: 'gafas de sol',
-		sound: 'assets/audio/gafas-de-sol.m4a',
+		word  : 'gafas de sol',
+		sound : 'assets/audio/gafas-de-sol.m4a'
 	},
 	{
-		word: 'galleta',
-		sound: 'assets/audio/galleta.m4a',
+		word  : 'galleta',
+		sound : 'assets/audio/galleta.m4a'
 	},
 	{
-		word: 'granja',
-		sound: 'assets/audio/granja.m4a',
+		word  : 'granja',
+		sound : 'assets/audio/granja.m4a'
 	},
 	{
-		word: 'grua',
-		sound: 'assets/audio/grua.m4a',
+		word  : 'grua',
+		sound : 'assets/audio/grua.m4a'
 	},
 	{
-		word: 'hamster',
-		sound: 'assets/audio/hamster.m4a',
+		word  : 'hamster',
+		sound : 'assets/audio/hamster.m4a'
 	},
 	{
-		word: 'helado',
-		sound: 'assets/audio/helado.m4a',
+		word  : 'helado',
+		sound : 'assets/audio/helado.m4a'
 	},
 	{
-		word: 'cielo',
-		sound: 'assets/audio/cielo.m4a',
+		word  : 'cielo',
+		sound : 'assets/audio/cielo.m4a'
 	},
 	{
-		word: 'huevo',
-		sound: 'assets/audio/huevo.m4a',
+		word  : 'huevo',
+		sound : 'assets/audio/huevo.m4a'
 	},
 	{
-		word: 'invierno',
-		sound: 'assets/audio/invierno.m4a',
+		word  : 'invierno',
+		sound : 'assets/audio/invierno.m4a'
 	},
 	{
-		word: 'libro',
-		sound: 'assets/audio/libro.m4a',
+		word  : 'libro',
+		sound : 'assets/audio/libro.m4a'
 	},
 	{
-		word: 'lista',
-		sound: 'assets/audio/lista.m4a',
+		word  : 'lista',
+		sound : 'assets/audio/lista.m4a'
 	},
 	{
-		word: 'manzana',
-		sound: 'assets/audio/manzana.m4a',
+		word  : 'manzana',
+		sound : 'assets/audio/manzana.m4a'
 	},
 	{
-		word: 'mariposa',
-		sound: 'assets/audio/mariposa.m4a',
+		word  : 'mariposa',
+		sound : 'assets/audio/mariposa.m4a'
 	},
 	{
-		word: 'mosquito',
-		sound: 'assets/audio/mosquito.m4a',
+		word  : 'mosquito',
+		sound : 'assets/audio/mosquito.m4a'
 	},
 	{
-		word: 'muñeca',
-		sound: 'assets/audio/muneca.m4a',
+		word  : 'muñeca',
+		sound : 'assets/audio/muneca.m4a'
 	},
 	{
-		word: 'museo',
-		sound: 'assets/audio/museo.m4a',
+		word  : 'museo',
+		sound : 'assets/audio/museo.m4a'
 	},
 	{
-		word: 'naríz',
-		sound: 'assets/audio/nariz.m4a',
+		word  : 'naríz',
+		sound : 'assets/audio/nariz.m4a'
 	},
 	{
-		word: 'nieve',
-		sound: 'assets/audio/nieve.m4a',
+		word  : 'nieve',
+		sound : 'assets/audio/nieve.m4a'
 	},
 	{
-		word: 'noche',
-		sound: 'assets/audio/noche.m4a',
+		word  : 'noche',
+		sound : 'assets/audio/noche.m4a'
 	},
 	{
-		word: 'ojos',
-		sound: 'assets/audio/ojos.m4a',
+		word  : 'ojos',
+		sound : 'assets/audio/ojos.m4a'
 	},
 	{
-		word: 'osito de goma',
-		sound: 'assets/audio/osito-de-goma.m4a',
+		word  : 'osito de goma',
+		sound : 'assets/audio/osito-de-goma.m4a'
 	},
 	{
-		word: 'pajaro',
-		sound: 'assets/audio/pajaro.m4a',
+		word  : 'pajaro',
+		sound : 'assets/audio/pajaro.m4a'
 	},
 	{
-		word: 'palmera',
-		sound: 'assets/audio/palmera.m4a',
+		word  : 'palmera',
+		sound : 'assets/audio/palmera.m4a'
 	},
 	{
-		word: 'pancakes',
-		sound: 'assets/audio/pancakes.m4a',
+		word  : 'pancakes',
+		sound : 'assets/audio/pancakes.m4a'
 	},
 	{
-		word: 'pasaporte',
-		sound: 'assets/audio/pasaporte.m4a',
+		word  : 'pasaporte',
+		sound : 'assets/audio/pasaporte.m4a'
 	},
 	{
-		word: 'patineta',
-		sound: 'assets/audio/patineta.m4a',
+		word  : 'patineta',
+		sound : 'assets/audio/patineta.m4a'
 	},
 	{
-		word: 'pelota',
-		sound: 'assets/audio/pelota.m4a',
+		word  : 'pelota',
+		sound : 'assets/audio/pelota.m4a'
 	},
 	{
-		word: 'perro',
-		sound: 'assets/audio/perro.m4a',
+		word  : 'perro',
+		sound : 'assets/audio/perro.m4a'
 	},
 	{
-		word: 'pescado',
-		sound: 'assets/audio/pescado.m4a',
+		word  : 'pescado',
+		sound : 'assets/audio/pescado.m4a'
 	},
 	{
-		word: 'pescador',
-		sound: 'assets/audio/pescador.m4a',
+		word  : 'pescador',
+		sound : 'assets/audio/pescador.m4a'
 	},
 	{
-		word: 'pinocho',
-		sound: 'assets/audio/pinocho.m4a',
+		word  : 'pinocho',
+		sound : 'assets/audio/pinocho.m4a'
 	},
 	{
-		word: 'pintar',
-		sound: 'assets/audio/pintar.m4a',
+		word  : 'pintar',
+		sound : 'assets/audio/pintar.m4a'
 	},
 	{
-		word: 'pizza',
-		sound: 'assets/audio/pizza.m4a',
+		word  : 'pizza',
+		sound : 'assets/audio/pizza.m4a'
 	},
 	{
-		word: 'pollo',
-		sound: 'assets/audio/pollo.m4a',
+		word  : 'pollo',
+		sound : 'assets/audio/pollo.m4a'
 	},
 	{
-		word: 'primavera',
-		sound: 'assets/audio/primavera.m4a',
+		word  : 'primavera',
+		sound : 'assets/audio/primavera.m4a'
 	},
 	{
-		word: 'profesor',
-		sound: 'assets/audio/profesor.m4a',
+		word  : 'profesor',
+		sound : 'assets/audio/profesor.m4a'
 	},
 	{
-		word: 'reloj',
-		sound: 'assets/audio/reloj.m4a',
+		word  : 'reloj',
+		sound : 'assets/audio/reloj.m4a'
 	},
 	{
-		word: 'saltar',
-		sound: 'assets/audio/saltar.m4a',
+		word  : 'saltar',
+		sound : 'assets/audio/saltar.m4a'
 	},
 	{
-		word: 'sol',
-		sound: 'assets/audio/sol.m4a',
+		word  : 'sol',
+		sound : 'assets/audio/sol.m4a'
 	},
 	{
-		word: 'sombrero',
-		sound: 'assets/audio/sombrero.m4a',
+		word  : 'sombrero',
+		sound : 'assets/audio/sombrero.m4a'
 	},
 	{
-		word: 'sonreír',
-		sound: 'assets/audio/sonreir.m4a',
+		word  : 'sonreír',
+		sound : 'assets/audio/sonreir.m4a'
 	},
 	{
-		word: 'submarino',
-		sound: 'assets/audio/submarino.m4a',
+		word  : 'submarino',
+		sound : 'assets/audio/submarino.m4a'
 	},
 	{
-		word: 'tambor',
-		sound: 'assets/audio/tambor.m4a',
+		word  : 'tambor',
+		sound : 'assets/audio/tambor.m4a'
 	},
 	{
-		word: 'techo',
-		sound: 'assets/audio/techo.m4a',
+		word  : 'techo',
+		sound : 'assets/audio/techo.m4a'
 	},
 	{
-		word: 'tele',
-		sound: 'assets/audio/tele.m4a',
+		word  : 'tele',
+		sound : 'assets/audio/tele.m4a'
 	},
 	{
-		word: 'teléfono',
-		sound: 'assets/audio/telefono.m4a',
+		word  : 'teléfono',
+		sound : 'assets/audio/telefono.m4a'
 	},
 	{
-		word: 'tesoro',
-		sound: 'assets/audio/tesoro.m4a',
+		word  : 'tesoro',
+		sound : 'assets/audio/tesoro.m4a'
 	},
 	{
-		word: 'tienda',
-		sound: 'assets/audio/tienda.m4a',
+		word  : 'tienda',
+		sound : 'assets/audio/tienda.m4a'
 	},
 	{
-		word: 'tostadora',
-		sound: 'assets/audio/tostadora.m4a',
+		word  : 'tostadora',
+		sound : 'assets/audio/tostadora.m4a'
 	},
 	{
-		word: 'triste',
-		sound: 'assets/audio/triste.m4a',
+		word  : 'triste',
+		sound : 'assets/audio/triste.m4a'
 	},
 	{
-		word: 'uvas',
-		sound: 'assets/audio/uvas.m4a',
+		word  : 'uvas',
+		sound : 'assets/audio/uvas.m4a'
 	},
 	{
-		word: 'vacaciones',
-		sound: 'assets/audio/vacaciones.m4a',
+		word  : 'vacaciones',
+		sound : 'assets/audio/vacaciones.m4a'
 	},
 	{
-		word: 'vaso',
-		sound: 'assets/audio/vaso.m4a',
+		word  : 'vaso',
+		sound : 'assets/audio/vaso.m4a'
 	},
 	{
-		word: 'zapato',
-		sound: 'assets/audio/zapato.m4a',
-	},
+		word  : 'zapato',
+		sound : 'assets/audio/zapato.m4a'
+	}
 ];
